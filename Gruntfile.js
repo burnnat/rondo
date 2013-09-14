b136bd62d9f80361eae71e6765c14ec206680073
Nat Burns <nbaccount@burnskids.com>
2013-09-14 15:25:37 -0400

    Re-enable Siesta tests after SauceLabs update.

--- Gruntfile.js
+++ Gruntfile.js
@@ -263,8 +263,7 @@ module.exports = function(grunt) {
 		"express:build",
 		"mochaTest:server",
 		"saucelabs-jasmine:mobile",
-		// Disable automated Siesta tests due to issues running on SauceLabs
-		// "saucelabs-siesta:mobile",
+		"saucelabs-siesta:mobile",
 		"saucelabs-selenium:mobile"
 	]);
 	
