65e86bd4425914bd0a5ced0aa1a145ba4845c2f8
Nat Burns <nbaccount@burnskids.com>
2015-04-26 20:29:03 -0400

    Fix vexflow generation in tutti package.

--- packages/local/tutti/vexflow/build.js
+++ packages/local/tutti/vexflow/build.js
@@ -20,17 +20,17 @@ var fileData = {};
 /**
  * Executes visitor on the object and its children (recursively).
  * The visitor function may return an array of children to visit.
- * 
+ *
  * @param {Object} node
  * @param {Function} visitor
  */
 var traverse = function(node, visitor) {
 	var next = visitor(node);
-	
+
 	if (!next) {
 		var key, child;
 		next = [];
-		
+
 		for (key in node) {
 			if (node.hasOwnProperty(key)) {
 				child = node[key];
@@ -40,7 +40,7 @@ var traverse = function(node, visitor) {
 			}
 		}
 	}
-	
+
 	next.forEach(
 		function(child) {
 			traverse(child, visitor);
@@ -52,22 +52,22 @@ var traverse = function(node, visitor) {
  * Parses a VexFlow class name from the given MemberExpression node.
  * If "strict" is enabled, the expression must reference the class
  * itself, not one of its members.
- * 
+ *
  * @param {Object} node
  * @param {Boolean} [strict]
- * 
+ *
  * @return {String}
  */
 var getVexClass = function(node, strict) {
 	var valid = true;
 	var segments = [];
-	
+
 	traverse(
 		node,
 		function(child) {
 			if (valid && child.type === 'Identifier') {
 				var name = child.name;
-				
+
 				// Class names are capitalized, but not entirely caps.
 				if (/[A-Z]/.test(name[0]) && name.toUpperCase() !== name) {
 					segments.push(child.name);
@@ -78,21 +78,21 @@ var getVexClass = function(node, strict) {
 			}
 		}
 	);
-	
+
 	if (strict && !valid) {
 		return null;
 	}
-	
+
 	if (segments.length > 0 && segments[0] === 'Vex') {
 		return segments.join('.');
 	}
-	
+
 	return null;
 };
 
 /**
  * Parses referenced and defined classes from a given piece of javascript.
- * 
+ *
  * @param {String} content
  * @param {Object} defs
  * @param {Object} refs
@@ -100,21 +100,21 @@ var getVexClass = function(node, strict) {
 var parseClasses = function(file, content) {
 	var data = fileData[file];
 	var ast = esprima.parse(content, { tolerant: true, loc: true, range: true });
-	
+
 	traverse(
 		ast,
 		function(node) {
 			if (node.type === 'AssignmentExpression') {
 				var left = node.left;
 				var right = node.right;
-				
-				if (left.type === 'MemberExpression') {
+
+				if (left.type === 'MemberExpression' || left.type === 'Identifier') {
 					var name = getVexClass(left, true);
-					
+
 					if (name) {
 						defined[name] = true;
 						data.defs[name] = true;
-						
+
 						if (/^Vex\.Flow\./.test(name)) {
 							data.refs['Vex.Flow'] = true;
 						}
@@ -122,22 +122,22 @@ var parseClasses = function(file, content) {
 							data.refs['Vex'] = true;
 						}
 					}
-					
+
 					return [node.right];
 				}
 			}
 			else if (node.type === 'MemberExpression') {
 				var name = getVexClass(node);
-				
+
 				if (name) {
 					data.refs[name] = true;
 				}
-				
+
 				return [];
 			}
 			else if (node.type === 'FunctionDeclaration') {
 				var name = getVexClass(node.id, true);
-				
+
 				if (name) {
 					defined[name] = true;
 					data.defs[name] = true;
@@ -148,26 +148,26 @@ var parseClasses = function(file, content) {
 };
 
 /**
- * Writes content for the given file, including headers for defined 
+ * Writes content for the given file, including headers for defined
  * and required classes ahead of the original file content.
- * 
+ *
  * @param {String} file
  * @param {Object} defs
  * @param {Object} refs
  */
 var writeWithHeader = function(file, callback) {
 	var data = fileData[file];
-	
+
 	if (!data) {
 		return callback();
 	}
-	
+
 	var combined = combine.create();
-	
+
 	// Setup header stream
 	var header = through().pause();
 	var eol = os.EOL;
-	
+
 	_.each(
 		_.sortBy(
 			_.filter(
@@ -181,43 +181,43 @@ var writeWithHeader = function(file, callback) {
 			header.queue('//@require ' + name + eol);
 		}
 	);
-	
+
 	_.each(
 		_.sortBy(_.keys(data.defs)),
 		function(name) {
 			header.queue('//@define ' + name + eol);
 		}
 	);
-	
+
 	header.end(eol);
-	
+
 	combined.append(header);
-	
+
 	// Setup source content stream
 	combined.append(getFileStream(file));
-	
+
 	// Add any files to be merged into this
 	if (data.merge) {
 		data.merge.forEach(function(child) {
 			combined.append(getFileStream(child));
 		});
 	}
-	
+
 	// Setup output stream
 	var dest = path.join(destDir, file);
 	mkdirp.sync(path.dirname(dest));
-	
+
 	var output = fs.createWriteStream(dest);
-	
+
 	output.on('error', function(err) {
 		console.error('Error writing file: ' + dest);
 		console.error(err);
 	});
-	
+
 	output.on('end', callback);
-	
+
 	combined.pipe(output);
-	
+
 	// Begin writing
 	header.resume();
 };
@@ -225,12 +225,12 @@ var writeWithHeader = function(file, callback) {
 var getFileStream = function(file) {
 	var src = path.join(srcDir, file);
 	var input = fs.createReadStream(src);
-	
+
 	input.on('error', function(err) {
 		console.error('Error reading file: ' + src);
 		console.error(err);
 	});
-	
+
 	return input;
 };
 
@@ -239,14 +239,14 @@ glob(
 	function(error, files) {
 		// Manually specify the font file we want in the build.
 		files.push(path.join(srcDir, 'fonts/vexflow_font.js'));
-		
+
 		async.series([
 			function(callback) {
 				async.each(
 					files,
 					function(src, next) {
 						var file = path.relative(srcDir, src);
-						
+
 						try {
 							fs.readFile(
 								src,
@@ -256,14 +256,14 @@ glob(
 										console.error('Error reading file: ' + file);
 										console.error(err);
 									}
-									
+
 									console.log('Processing file: ' + file);
-									
+
 									fileData[file] = {
 										defs: {},
 										refs: {}
 									};
-									
+
 									parseClasses(file, data);
 									next();
 								}
@@ -280,21 +280,31 @@ glob(
 				// Merge table properties into VexFlow namespace file
 				var parent = 'flow.js';
 				var child = 'tables.js';
-				
+
 				if (fileData[parent] && fileData[child]) {
 					fileData[parent].merge = [child];
 					delete fileData[child];
 				}
-				
+
 				// Ignore circular reference
-				var data = fileData['notehead.js'];
-				
-				if (data) {
-					delete data.refs['Vex.Flow.StaveNote'];
-				}
-				
+				var circular = {
+					'notehead.js': 'Vex.Flow.StaveNote',
+					'music.js': 'Vex.Flow.KeyManager'
+				};
+
+				_.each(
+					circular,
+					function(ref, file) {
+						var data = fileData[file];
+
+						if (data) {
+							delete data.refs[ref];
+						}
+					}
+				);
+
 				console.log('Writing data...');
-				
+
 				async.each(
 					files,
 					function(src, next) {
